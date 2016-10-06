0f1fe23d35b18eac5120aab559665f3bb5ed57aa
Matthew Markic <matt.markic@hotmail.com>
2016-10-06 17:00:25 +0000

    sorting exercises using anonymous callback functions

---
+++ sorting.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Sorting exercise 1
+const array = [10, 2, 5, 1, 9];
+array.sort(function(a,b) {
+  return a - b
+});
+
+// Advanced sorting exercise
+const students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b) {
+  if (a.name > b.name) {
+    return 1
+  }
+  else if (a.age < b.age) {
+    return 1
+  }
+});
